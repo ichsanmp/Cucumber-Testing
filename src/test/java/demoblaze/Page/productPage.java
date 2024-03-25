aceaed096571181cae9e2fdcb3cd41adcd4dac34
ichsanmp <impratama04@gmail.com>
2024-03-25 21:18:27 +0700

    [FIX] modify productPage for fixing test

--- src/test/java/demoblaze/Page/productPage.java
+++ src/test/java/demoblaze/Page/productPage.java
@@ -2,12 +2,20 @@ package demoblaze.Page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 import static demoblaze.Helper.utility.driver;
 
 public class productPage {
+    WebDriverWait wait;
+
+    public productPage() {
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
 
     By text_productDetPage(String product) {
         return By.xpath("//div[@id='tbodyid']/descendant::h2[text()='"+product+"']");
@@ -19,6 +27,7 @@ public class productPage {
     }
 
     public void clickAddToCartButton() throws InterruptedException {
+        wait.until(ExpectedConditions.presenceOfElementLocated(addToCartButton));
         driver.findElement(addToCartButton).click();
         Thread.sleep(5000);
     }
