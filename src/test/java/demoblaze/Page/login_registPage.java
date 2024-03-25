88139fef412e83ad1178d32ef60a0d237aeab5a0
ichsanmp <impratama04@gmail.com>
2024-03-25 17:10:41 +0700

    [FIX] modify loginPage for fixing test

--- src/test/java/demoblaze/Page/login_registPage.java
+++ src/test/java/demoblaze/Page/login_registPage.java
@@ -1,6 +1,11 @@
 package demoblaze.Page;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static demoblaze.Helper.utility.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,6 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class login_registPage {
 
     String username, pass;
+    WebDriverWait wait;
+
+    public login_registPage() {
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
 
     By signup_inputUsername = By.xpath("//input[@id='sign-username']");
     By signup_inputPassword = By.xpath("//input[@id='sign-password']");
@@ -50,6 +60,7 @@ public class login_registPage {
         Thread.sleep(5000);
     }
     public void validateUsernameDisplayed() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(validateLoginNavbar));
         String usernameDisplay = driver.findElement(validateLoginNavbar).getText();
         assertEquals("Welcome " + username, usernameDisplay);
     }
